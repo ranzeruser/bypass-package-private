a46b9f6bee363cbc55c4381376586153be7d2629
Lingfeng <2354833@qq.com>
2019-06-27 23:18:42 +0800

    绕过包级私有限制

---
+++ src/main/java/com/github/blindpirate/extensions/BridgeClass.java
@@ -0,0 +1,11 @@
+package com.github.blindpirate.extensions;
+
+public class BridgeClass{
+
+    public CaptureSystemOutputExtension getException(){
+        return new CaptureSystemOutputExtension();
+    }
+
+
+
+}
