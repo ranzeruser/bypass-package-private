eb527e5f04914288d21c1867278159039783bdb3
王文哲 <mysteryven@gmail.com>
2020-01-19 10:50:48 +0800

    跨越包限制的方法实战

---
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemCall.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class CaptureSystemCall {
+    public static Object newCaptureSystemCall() {
+        return new CaptureSystemOutputExtension();
+    }
+}
